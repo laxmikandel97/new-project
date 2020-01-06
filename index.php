632d3038d549b0d4461a59bb99f4f0f8365af820
Laxmi Kandel <laxmikandel97@gmail.com>
2020-01-06 14:08:55 -0500

    Created index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/** create by phpstrom */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<h1> New Project </h1>
+</body>
+</html>
